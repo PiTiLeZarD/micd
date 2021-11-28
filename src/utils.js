3d8c973e11d32b3c43d85d2dba11192af57e3837
Jonathan Adami <pitilezard@gmail.com>
2021-11-28 14:33:48 +1000

    Remove the log here

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,6 @@ import { Vector2 } from "three";
 
 export const rotateTexture = (texture, angle) => {
     const rotatedTexture = texture.clone();
-    console.log(texture);
     rotatedTexture.rotation = (angle * Math.PI) / 180;
     rotatedTexture.needsUpdate = true;
     rotatedTexture.center = new Vector2(0.5, 0.5);
